aa8cfcd246480388905c11957757b786eccfc94c
Woolf <woolf.no1@gmail.com>
2025-06-22 17:21:20 +0500

    added settings for ttl cache

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,9 @@
+import re
+from datetime import timedelta
 from functools import lru_cache
+from typing import Any
 
+from pydantic import field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -17,12 +21,37 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE: int = 10
 
+    TTL_CACHE_EXTERNAL_API: timedelta = timedelta(seconds=0)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     @property
     def ASYNC_DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.db_user}:{self.db_pass}@{self.db_host}:{self.db_port}/{self.db_name}"
+
+    @field_validator('TTL_CACHE_EXTERNAL_API', mode='plain')
+    @classmethod
+    def is_ttl_cache_external_api(cls, value: Any) -> timedelta:
+        if isinstance(value, timedelta):
+            return value
+        if not isinstance(value, str):
+            raise ValueError(f'{value} is not valide type, must be string')
+        pattern = "(^\d*)([s,m,h,d]$)"
+        r = re.compile(pattern)
+        m = r.match(value)
+        if m:
+            v = float(m[1])
+            match m[2]:
+                case 's':
+                    return timedelta(seconds=v)
+                case 'm':
+                    return timedelta(minutes=v)
+                case 'h':
+                    return timedelta(hours=v)
+                case 'd':
+                    return timedelta(days=v)
+        raise ValueError(f'{value} is not valide')
 
 
 @lru_cache
--- app/utils/external_api.py
+++ app/utils/external_api.py
@@ -1,19 +1,23 @@
+import inspect
 import json
 import pathlib
-import inspect
 from datetime import timedelta
 from functools import lru_cache
+
 from aiohttp import ClientSession
+
+from app.core.config import get_settings
 from app.utils.errors import CurrencyNotAvailableError
 from app.utils.lru_cache_ttl import lru_cache_ttl
 
 url = 'https://www.cbr-xml-daily.ru/daily_json.js'
 
-@lru_cache_ttl(ttl_delta=timedelta(minutes=30))
+
+@lru_cache_ttl(ttl_delta=get_settings().TTL_CACHE_EXTERNAL_API)
 async def get_currency():
     async with ClientSession() as client:
         async with client.get(url=url) as responce:
-            if responce.status==200:
+            if responce.status == 200:
                 return await responce.json(content_type="application/javascript", encoding="utf-8")
             else:
                 raise CurrencyNotAvailableError()
@@ -23,9 +27,10 @@ async def get_currency():
 #     with open(base_dir.joinpath("daily_json.js"), "r", encoding="utf-8") as f:
 #         return json.load(f)
 
+
 async def currency_list():
     currency = await get_currency()
     cur = dict()
     for key, val in currency["Valute"].items():
-        cur[key]=val["Name"]
+        cur[key] = val["Name"]
     return cur
