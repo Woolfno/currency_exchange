e0054210431bc5c6422db36d0c18108a70fcc0b9
Woolf <woolf.no1@gmail.com>
2025-06-22 13:55:58 +0500

    add decorator lru cache with ttl

---
+++ app/utils/lru_cache_ttl.py
@@ -0,0 +1,26 @@
+import functools
+import time
+from datetime import datetime, timedelta
+from functools import lru_cache
+
+
+def lru_cache_ttl(maxsize=128, typed=False, ttl_delta: timedelta = timedelta(hours=1)):
+    def decorator(func):
+        cache = dict()
+
+        def wrapper(*args, **kwargs):
+            key = functools._make_key(args, kwargs, typed)
+
+            current_time = time.time()
+            if key in cache:
+                value, expiration_time = cache[key]
+                if current_time < expiration_time:
+                    return value
+                else:
+                    del cache[key]
+            result = func(*args, **kwargs)
+            cache[key] = (result, (datetime.now() + ttl_delta).timestamp())
+            return result
+
+        return wrapper
+    return decorator
